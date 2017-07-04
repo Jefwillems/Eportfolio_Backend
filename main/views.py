cf472e2ddf534020ce7f8612cbe2c1ccf9132aa0
Jefwillems <jefwillems100@gmail.com>
2017-07-04 20:49:38 +0200

    initial commit

--- main/views.py
+++ main/views.py
@@ -9,5 +9,5 @@ class HomeView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['welcome'] = "Hello world"
+        context['welcome'] = "Hello world!"
         return context
