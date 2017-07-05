97fab2310e1c62dd06a415d91541e664ce492a44
Jefwillems <jefwillems100@gmail.com>
2017-07-05 10:08:10 +0200

    removed index View for redirect to /api

--- main/views.py
+++ main/views.py
@@ -1,13 +1,8 @@
 from django.views import generic
 
 
-# Create your views here.
+class HomeView(generic.RedirectView):
+    permanent = False
 
-
-class HomeView(generic.TemplateView):
-    template_name = 'main/home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['welcome'] = "Hello world!"
-        return context
+    def get_redirect_url(self, *args, **kwargs):
+        return '/api'
